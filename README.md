0adbbae3bca48cbe3d739a6eba4f59cd7a2ca0db
Erik Williams <erwilliams642003@gmail.com>
2024-01-13 23:44:34 -0900

    erikwilliams@Eriks-Air scheduler-checker % node . Example app listening at http://localhost:3000 Navigate to http://localhost:3000/monthlyCheck to trigger the function. Cloud Scheduler triggered this function. Count: 1 Cloud Scheduler triggered this function. Count: 2 Cloud Scheduler triggered this function. Count: 3

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # scheduler-checker
 
-ctrl+c to exit node . tirggered example app
+This is where you will see the confirmation:
+http://localhost:3000/monthlyCheck
+
+
+fact of the day: Use ctrl+c to exit the "node ." triggered example app
--- index.js
+++ index.js
@@ -1,25 +1,63 @@
-// exports.monthlyCheck = (req, res) => {
-//     console.log("Cloud Scheduler triggered this function.");
-//     res.status(200).send("Function executed successfully!");
-//   };
+// // // This is what will be executed on the cloudfunction to test if the trigger code is working:
+// // exports.monthlyCheck = (req, res) => {
+// //     console.log("Cloud Scheduler triggered this function.");
+// //     res.status(200).send("Function executed successfully!");
+// //   };
   
+// // // This next portion is for running in VScode to test the timer
+// // If it is runnin properly, we should see an increment every trigger from the cloudfunction
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// // add route to make it easier to find the success message:
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the root of the Express server!');
+// });
+
+
+// app.get('/monthlyCheck', (req, res) => {
+//   console.log("Cloud Scheduler triggered this function.");
+//   res.status(200).send("Function executed successfully!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+//   console.log("navigate to: http://localhost:3000/monthlyCheck");
+// });
+
+// let triggerCount = 0;
+
+// app.get('/monthlyCheck', (req, res) => {
+//   triggerCount++;
+//   console.log(`Cloud Scheduler triggered this function. Count: ${triggerCount}`);
+//   res.status(200).send(`Function executed successfully! Count: ${triggerCount}`);
+// });
+
+// New code-v
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// add route to make it easier to find the success message:
+// Counter to keep track of the number of times the endpoint is triggered
+let triggerCount = 0;
+
+// Route for the root of the server
 app.get('/', (req, res) => {
   res.send('Welcome to the root of the Express server!');
+  res.send(`"Cloud Scheduler tri ggered this function. This URL has been triggered this many times:" ${triggerCount}`) // For some reason the text is not on the webpage...
 });
 
-
+// Route to mimic the Cloud Scheduler trigger with a count
 app.get('/monthlyCheck', (req, res) => {
-  console.log("Cloud Scheduler triggered this function.");
-  res.status(200).send("Function executed successfully!");
-  console.log("http://localhost:3000/monthlyCheck");
+  triggerCount++;
+  console.log(`Cloud Scheduler triggered this function. Count: ${triggerCount}`);
+  res.status(200).send(`Function executed successfully! Count: ${triggerCount}`);
 });
 
+// Start the Express server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Navigate to http://localhost:3000/monthlyCheck to trigger the function.");
 });
